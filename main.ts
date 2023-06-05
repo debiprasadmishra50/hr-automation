5708faf70728199130405802210343d8bd44ef08
Debi Prasad Mishra <debiprasadmishra50@gmail.com>
2023-06-05 16:57:40 +0530

    WIP

--- main.ts
+++ main.ts
@@ -69,7 +69,7 @@ const qod = schedule("45 8 * * *", async () => {
 });
 
 // run at 09:00 AM everyday
-const dobAndDoj = schedule("55 16 * * *", async () => {
+const dobAndDoj = schedule("29 11 * * *", async () => {
   console.log("[+] Cron DOB+DOJ running at 09:00AM");
 
   await main();
